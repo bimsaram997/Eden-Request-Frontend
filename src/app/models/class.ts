0d801512b50ef6f631eb46bfd95c912bb6a9e1bb
Bimsara <bimsara.anaththapathiranage@tuni.fi>
2026-07-11 22:37:59 +0300

    changes on request details comop

--- src/app/models/class.ts
+++ src/app/models/class.ts
@@ -2,7 +2,7 @@ export interface Employee { id: number; name: string; role: string; }
 export interface ItemCategory { id: number; name: string; }
 export interface Item { id: number; name: string; itemCategoryId: number; category?: ItemCategory; }
 export interface RequestLine { id?: number; itemId: number; item?: Item; quantity: number; unitType: 'Pcs' | 'Trolley'; }
-export interface RequestHeader { id?: number; roomNumber: string | null; status: 'Pending' | 'Acknowledged' | 'Delivered'; employeeId: number; employee?: Employee; updatedBy?: Employee; roomListId: number; lines: RequestLine[]; createdAt?: string; updatedAt?: string;  notes?: string; }
+export interface RequestHeader { id?: number; roomNumber: string | null; status: 'Pending' | 'Acknowledged' | 'Delivered'; employeeId: number; employee?: Employee; updatedBy?: Employee | null; roomListId: number; lines: RequestLine[]; createdAt?: string; updatedAt?: string;  notes?: string; }
 export interface UserSession { id: number; name: string; role: 'Housekeeper' | 'TeamLeader'; }
 export interface PlaceBulkRequestDto { employeeId: number; roomListId: number; roomNumber: string | null; items: { itemId: number; quantity: number; unitType: string; }[]; }
 export interface EmployeeDto {
